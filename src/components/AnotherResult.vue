2dbfabe55234d571df1a0d9780ada53288f0fc86
Milan <mistikone1@gmail.com>
2020-06-30 16:34:17 +0200

    vuex

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,13 +1,23 @@
 <template>
-  <p>Counter is: {{ counter }}</p>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of clicks {{ stringCounter}}</p>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
+  // computed: {
+  //   counter() {
+  //     return this.$store.getters.doubleCounter;
+  //   },
+  //   clicks() {
+  //     return this.$store.getters.stringCounter;
+  //   }
+  // }
   computed: {
-    counter() {
-      return this.$store.state.counter * 2;
-    }
+    ...mapGetters(["doubleCounter", "stringCounter"])
   }
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,15 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     counter: 0,
+  },
+
+  getters: {
+    doubleCounter: state => {
+      return state.counter * 2;
+    },
+
+    stringCounter: state => {
+      return state.counter + ' clicks.';
+    }
   }
 })
