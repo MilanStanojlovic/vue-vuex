6144a8f0347980b4334724eea509cbfd957bd492
Milan <mistikone1@gmail.com>
2020-07-01 12:57:26 +0200

    vue vuex

---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="asyncIncrement({by: 50, duration: 500})">Increment</button>
+    <button class="btn btn-primary" @click="asyncDecrement({by: 50, duration: 500})">Decrement</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  methods: {
+    ...mapActions(["asyncIncrement", "asyncDecrement"])
+  }
+  //mapActions creates these methods
+  // increment(value) {
+  //   this.$store.dispatch("increment", value);
+  // }
+};
+</script>
